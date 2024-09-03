e88161fb2eca10cfe0fb1e94f316138093f76e0b
Kizzie <110618945+Anonymous56Risk@users.noreply.github.com>
2024-09-03 22:33:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(6497757690)
+ADMINS.append(6303569936)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
